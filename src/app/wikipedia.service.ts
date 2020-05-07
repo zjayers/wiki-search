bf1d5e97eecf45021d0a1b0e2e119249cb647bb0
Zach Ayers <zayers.dev@gmail.com>
2020-05-06 19:32:29 -0500

    Refactored rxjs

--- src/app/wikipedia.service.ts
+++ src/app/wikipedia.service.ts
@@ -1,22 +1,38 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { pluck } from 'rxjs/operators';
+
+interface WikipediaResponse {
+  query: {
+    search: Array<{
+      title: string;
+      snippet: string;
+      pageid: number;
+    }>
+  };
+}
 
 @Injectable({
   providedIn: 'root',
 })
 export class WikipediaService {
+
   constructor(private http: HttpClient) {}
 
   public search(term: string) {
-    return this.http.get('https://en.wikipedia.org/w/api.php', {
-      params: {
-        action: 'query',
-        format: 'json',
-        list: 'search',
-        utf8: '1',
-        srsearch: term,
-        origin: '*',
-      },
-    });
+    return this.http
+      .get<WikipediaResponse>('https://en.wikipedia.org/w/api.php', {
+        params: {
+          action: 'query',
+          format: 'json',
+          list: 'search',
+          utf8: '1',
+          srsearch: term,
+          origin: '*',
+        },
+      })
+      .pipe(
+        pluck('query', 'search')
+      );
   }
 }
